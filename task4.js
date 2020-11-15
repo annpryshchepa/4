2f236d75b588ac195e85bfe7f213fa33725eb56a
annpryshchepa <pryshchepa.hanna@gmail.com>
2020-11-15 21:45:56 +0200

    modified task

--- task4.js
+++ task4.js
@@ -1,7 +1,11 @@
-function nextInLine(arr, item) {
-    // Your code here
-    arr.push(item);
-    var removed = arr.shift();
-    return removed; // Change this line
-  }
-  console.log(nextInLine(1,2,3,4,5, 9))
+// Write a function nextInLine which takes an array (arr) and a number (item) as arguments. 
+//Add the number to the end of the array, then remove the first element of array. 
+//The `nextInLine` function should then return the element that was removed.
+
+function nextInLine(arrNumber, item) {
+    let firstNumber = arrNumber[0];
+    arrNumber.shift();
+    console.log(firstNumber);           // return arr.shift();
+    arrNumber.push(item);
+}
+console.log(nextInLine(2, 1))
